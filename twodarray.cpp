2c803df17ca6fbd17836041a5f2c5fcb367d8caf
13poorvi03 <poorvisharma0306@gmail.com>
2025-12-14 13:43:35 +0530

    updated files

---
+++ twodarray.cpp
@@ -0,0 +1,11 @@
+//it have basically two or more dimentions. the two dimentional array is also called matrix 
+
+//datatype array_name[r][c]
+
+// this is a 2D array where r depicts number of rows in matrics and depicts number of columns in the matrix 
+
+///       0  1  2  3  4  5
+//  phy  12 23 33 54 66 77
+// chem  77 34 22 89 76 54
+// maths 78 99 32 98 43 22
+
