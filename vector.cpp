b1e227de1197498023d00a179f4e1840c640b4da
13poorvi03 <poorvisharma0306@gmail.com>
2025-12-15 11:43:19 +0530

    update the code

--- vector.cpp
+++ vector.cpp
@@ -46,4 +46,101 @@
 
 
 
-5. add elements
+// 5. add elements
+
+//v.push back(element)
+//push_back is a memeber function of the c++ vector class that adds a new element at the end of the vector increasing its size by one 
+//ex- vector<int> v;
+// v.push_back(10);   // v = {10}
+// v.push_back(20);   // v = {10, 20}
+
+
+// 6. insert operation 
+
+//v.insert(position,element)
+//insert()is a vector function in c++ that adds an elements at a specified position inside the vector 
+//all existing elements from that position onward are shifted one step to the right 
+//position -- an iterator pointing to the location where the new elements should be inserted 
+//elements -- the value to be inserted 
+
+//ex ---vector<int> v = {10, 20, 30};
+// v.insert(v.begin() + 1, 99);  
+// Result: {10, 99, 20, 30}
+
+
+// 6.  begin operation 
+
+//v.begin();
+// begin() is a member function of the c++ vector class that returns an iterator pointing ro the first elements of the vector 
+//it returns  an iterator to the first element 
+
+//ex----vector<int> v = {10, 20, 30};
+// auto it = v.begin();   // it points to 10
+
+
+
+// 7. end operation 
+
+//v.end();
+//end() is a vector function that return an iterator pointing to the position just after the last elemnts of the vector 
+// it does not point to the last elements
+//it point to a past the end position (an valid elemnet)
+//used mainly for iteration and alogrithm 
+
+//ex---vector<int> v = {10, 20, 30};
+// auto it = v.end();   // points after 30 (not to 30)
+
+
+
+// 8. delete operation 
+
+//v.pop_back();
+//pop_back() is a vector function in c++ that removes the last elements from the vector .
+//it reduces the size by 1 but does not return the revoved element.
+//it delete the last elemnt of the vector 
+//capacity usually remains unchanges 
+//safe only if the vector is not empty
+
+//ex----vector<int> v = {10, 20, 30};
+// v.pop_back();  
+// v becomes {10, 20}
+
+
+// 9. erase operation 
+
+//v.erase(position);
+//erase() is a vector function in c++ that removes the elemnt at he given position (specified using an iterator )
+//all elemnts after that position shift one step left to the gap 
+//position -- an iterator pointing to the elemnt you want to delete 
+
+//ex---vector<int> v = {10, 20, 30, 40};
+// v.erase(v.begin() + 1);  
+// Result: {10, 30, 40}
+
+
+// 10. operation 
+
+//v.erase(v.end()-2);
+// v.end() points one position past the last elements
+//so v,end()-2 points to the second last elem of the vector 
+// v.erase(v.end()-2); removes the second last elem of the vector
+
+//ex---vector<int> v = {10, 20, 30, 40, 50};
+// v.erase(v.end() - 2);  
+// Removes 40 (second last element)
+
+// Result: {10, 20, 30, 50}
+
+
+// 11. clear operation 
+
+//v.clear();
+//clear() is a vector function in c++ that removes all elements from the vector making its size 0;
+//delete all elements instide the vector 
+//size becomes 0 but,
+//capacity remains unchanged (memory is not feed)
+
+//ex--vector<int> v = {10, 20, 30};
+// v.clear();
+// // v becomes {}
+// // v.size() = 0
