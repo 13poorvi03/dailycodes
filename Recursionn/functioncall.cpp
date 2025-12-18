ff23ce4f773b6cdf27294facfaafcb92eeb2de74
13poorvi03 <poorvisharma0306@gmail.com>
2025-12-18 21:54:00 +0530

    added

---
+++ Recursionn/functioncall.cpp
@@ -0,0 +1,10 @@
+// Recursion is a method where a function solves a problem by calling itself on smaller inputs until a base case is reached.**
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    return 0;
+}
