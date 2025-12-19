0f174c0f9ab7185060b7a533dfed9cae627a4f3c
13poorvi03 <poorvisharma0306@gmail.com>
2025-12-19 23:48:55 +0530

    updated

--- Recursionn/power.cpp
+++ Recursionn/power.cpp
@@ -1,7 +1,26 @@
 //make a function which calculates 'a' raised to the power 'b' using recursion .
 
+#include<iostream>
+using namespace std;
+  int power(int a, int b){
+    if(b==0 ) return 1;
+    return a*power(a,b-1);
+  }
 
+int main(){
+  int a;
+  cout<<"enter the base : ";
+  cin>>a;
 
+  int b;
+  cout<<"enter the power : ";
+  cin>>b;
+
+  int p = power(a,b);
+  cout<<p;
+
+    return 0;
+}
 
 
 
