29c1a1c3eca2935466e18d1dc919f9c7e2553b95
13poorvi03 <poorvisharma0306@gmail.com>
2025-12-19 14:30:50 +0530

    added

---
+++ Recursionn/printdecinc.cpp
@@ -0,0 +1,21 @@
+//print decreasing -increasing 
+
+#include<iostream>
+using namespace std;
+void increasing(int n){
+  if(n==0) return ;
+
+     cout<<n<<endl;
+    increasing(n-1);
+     cout<<n<<endl;
+    return ;
+  
+}
+int main(){
+  
+  int n;
+  cout<<"enter the number : ";
+  cin>>n;
+  increasing(n);
+  return 0;
+}
