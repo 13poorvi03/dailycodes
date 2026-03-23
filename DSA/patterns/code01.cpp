dc3c3878d50f3a8ecb978540c15151e18edd6395
13poorvi03 <poorvisharma0306@gmail.com>
2026-03-23 23:58:05 +0530

    added

--- DSA/patterns/code01.cpp
+++ DSA/patterns/code01.cpp
@@ -17,13 +17,33 @@
 
 
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n = 5;
+//     for(int i=1; i<n;i++){  //outer for lines for coloumn 
+//         for(int j=1; j<n; j++){  //inner for rows
+//          cout<<j;
+
+//         }
+//         cout<<endl;
+//     }
+   
+//     return 0;
+// }
+
+
+
+
 #include<iostream>
 using namespace std;
 int main(){
     int n = 5;
-    for(int i=1; i<n;i++){  //outer for lines for coloumn 
-        for(int j=1; j<n; j++){  //inner for rows
-         cout<<j;
+    for(int i=0; i<n;i++){  //outer for lines for coloumn
+        char ch = 'A'; 
+        for(int j=0; j<n; j++){  //inner for rows
+         cout<<ch;
+         ch = ch + 1;
 
         }
         cout<<endl;
